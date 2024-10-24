44fe4ced0d40cda3177a7c39eb3c798d36482fb0
Proportion1 <proportional@msn.com>
2024-10-24 18:13:23 +1000

    added asteroid field

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
@@ -7,14 +10,44 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
+    #added player before so there is something to add to the updateables and drawables group
+    #player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    updateables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updateables, drawables)
+    Asteroid.containers = (asteroids, updateables, drawables)
+    AsteroidField.containers = (updateables,)
+    #added after because the lesson says to create all player objects after the change
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    #asteroid = Aster
+    asteroid_field =  AsteroidField()
+    
+
+    
+    #for player in updateables:
+    #    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    #for player in drawables:
+    #    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        milliseconds = clock.tick(60)
+        dt = milliseconds / 1000
+        for updates in updateables:
+            updates.update(dt)
+
+        for draws in drawables:
+            draws.draw(screen)
         pygame.display.flip()
 
-    
 
 if __name__ == "__main__":
     main()
